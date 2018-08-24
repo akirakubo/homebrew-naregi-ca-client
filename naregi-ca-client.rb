a50fd512704202ee0b8cc7b787a8c55723722bfa
akirakubo <akirakubo@users.noreply.github.com>
2018-08-24 19:14:44 +0900

    Remove comments

--- naregi-ca-client.rb
+++ naregi-ca-client.rb
@@ -1,35 +1,19 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class NaregiCaClient < Formula
   desc ""
   homepage ""
   url "https://ca-dev.naregi.org/repos/source/stable/naregi-ca-3.2.1.tar.bz2"
   sha256 "3c4185ebdb375dbf96a0539a8fd047fa2f745e722ef876f5acda0286fe72a259"
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "client" # if this fails, try separate make/make install steps
+    system "make", "client"
     system "make", "install-client"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test naregi-ca`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
